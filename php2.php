891de96fbc0804e3797ee94451ca11f919ce487c
Brendo Almeida <72768968+brandonspecter1618@users.noreply.github.com>
2022-05-12 15:13:09 -0300

    save

---
+++ php2.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+
+<html lang="pt-BR">
+    <head>
+        <meta charset="utf-8">
+        <meta name="describes" content="HTML55">
+        <meta name="autor" content="Brandon Specter">
+
+        <link href="/css/subcss/php2_css.css" rel="stylesheet">
+        <link href="responsive/responsive.css" rel="stylesheet">
+
+        <title>PHP1</title>
+        
+                        <!--Font for Headers-->
+        <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
+
+
+                        <!--Font for Paragraph-->
+
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap" rel="stylesheet">
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    </head>
+
+<body>
+
+    <div class="var_block_menu"></div>
+
+
+    <div class="position_menu">
+        <nav role="navigation" class="primary-navigation">
+            <ul>
+                <li><a href="index.html">Home</a></li>
+                <li><a href="#">Produtos</a>
+                    <ul>
+                        <li><a href="#">Web Development</a></li>
+                        <li><a href="#">Web Design</a></li>
+                        <li><a href="#">Illustration</a></li>
+                        <li><a href="#">Iconography</a></li>
+                    </ul>
+                </li>
+                <li><a href="php_java.html">PHP/JAVA</a>
+                    <ul>
+                        <li><a href="php1.html">PHP1</a></li>
+                        <li><a href="#">PHP2</a></li>
+                        <li><a href="#">PHP3</a></li>
+                        <li><a href="#">PHP4</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Contatos</a></li>
+            </ul>
+        </nav>
+    </div>
+
+
+
+    <!--
+    
+    <form action="phptest.php"  method="POST">
+
+        <input type="text" name="name">
+        <input type="text" name="email">
+
+        <input type="submit" name="submit">
+    </form>
+
+    -->
+
+
+    <div class="var_p">
+    
+    <?php
+
+        $txt = "Learn PHP";
+        echo($txt . "<br><br><br>");
+
+        $a = "Hello Brandon";
+        $a = null;
+        
+        var_dump($a);
+
+        echo($a);
+
+        ?>
+
+    </div>
+
+    
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+
+
+</body>
+</html>
+
+
+
+
+
+
+
---
+++ phptest.php
@@ -0,0 +1,8 @@
+<?php
+
+    $a = $_POST['num1'];
+    $b = $_POST['num2'];
+
+    $total = $a + $b;
+
+    echo($total);
